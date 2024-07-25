5bffd5f6d5d3ca31e8b22a68a27ff5c78c486abc
mbourke525 <mitchell.bourke@ausproof.com.au>
2024-07-26 09:38:43 +1000

    .

--- system.nim
+++ system.nim
@@ -1,6 +1,5 @@
 #Implements utility functions and macros for system operations
-import std/envvars
-
+import std/[envvars, macros, strutils, sequtils]
 #Takes a string containing 1 or more enviromental variables and replaces them with there values before returning the string
 func parseEnvString(str: string): string {.inline.} =
     var result = ""
@@ -26,3 +25,78 @@ func parseEnvString(str: string): string {.inline.} =
         i += 1
 
     return result  
+
+
+#Takes a path and splits it into its folders/file
+macro splitPath(str: string): seq[string] =
+    quote do:
+        var pathResult: seq[string] = @[]
+        var buff = ""
+
+        for i in 0..<`str`.len:
+            if `str`[i] == '/' or `str`[i] == '\\':
+                if buff.len > 0:
+                    pathResult.add(buff)
+                    buff = ""
+            else:
+                buff.add(`str`[i])
+
+        if buff.len > 0:
+            pathResult.add(buff)
+
+        pathResult
+
+#Splits a string into the filename/path and the extension
+macro splitExt(str: string): seq[string] =
+    quote do:
+        var extResult: seq[string] = @[]
+        var buff = ""
+
+        #Iterate backwards until we find the first . or a folder seperator
+        var i=`str`.len-1
+        while i >= 0:
+            if `str`[i] == '.':
+                if buff.len > 0:
+                    extResult.add(buff)
+                    buff = ""
+            elif `str`[i] == '/' or `str`[i] == '\\':
+                break
+            else:
+                buff = `str`[i] & buff#This is backwards to get the extension in the correct order
+
+            i = i - 1
+
+        #Now if there is any remaining data then add it to the first element
+        while i >= 0:
+            buff = `str`[i] & buff
+            i -= 1
+
+        #Now add the filename to the result at the start of the list (even if its blank)
+        buff & extResult
+
+#Splits a string into the folder path and filename
+macro splitFolder(str: string): seq[string] =
+    quote do:
+        var folderResult: seq[string] = @[]
+        var buff = ""
+
+        #Iterate backwards until we find the first folder seperator
+        var i=`str`.len-1
+        while i >= 0:
+            if `str`[i] == '/' or `str`[i] == '\\':
+                if buff.len > 0:
+                    folderResult.add(buff)
+                    buff = ""
+                break
+            else:
+                buff = `str`[i] & buff#This is backwards to get the extension in the correct order
+
+            i = i - 1
+
+        #Now if there is any remaining data then add it to the first element
+        while i >= 0:
+            buff = `str`[i] & buff
+            i -= 1
+
+        #Now add the filename to the result at the start of the list (even if its blank)
+        buff & folderResult
