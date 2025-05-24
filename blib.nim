f22954ad8a8ac45ef4117e949ee3bfa36ef1a91b
mbourke525 <mitchell.bourke@ausproof.com.au>
2025-05-24 22:58:23 +1000

    .

--- blib.nim
+++ blib.nim
@@ -6,23 +6,25 @@ when not declared(with):
     include "./utils.nim"
     include "./str_utils.nim"
     include "./num_utils.nim"
-    include "./config.nim"
-    include "./system.nim"
     include "./seq_utils.nim"
     include "./hashing.nim"
     include "./binaryunits.nim"
 
+when defined(linux) or defined(macosx) or defined(windows):    
+    include "./system.nim"    
+    include "./config.nim"
 
-    #Incldue the arduino specific functions only when the arduino flag is set
-    when declared(arduino):
-        include "./arduino/arduino.nim"
+#Incldue the arduino specific functions only when the arduino flag is set
+when declared(arduino):
+    include "./arduino/arduino.nim"
 
-    #The standard library time functions dont work properly on 8bit microcontrollers, i havent tested them on 16bit but expect this to fail as well due to assumption that int can be used to store an i32
-    when sizeof(int) >= 4:
-        import "./time.nim"
+#The standard library time functions dont work properly on 8bit microcontrollers, i havent tested them on 16bit but expect this to fail as well due to assumption that int can be used to store an i32
+when sizeof(int) >= 4:
+    import "./time.nim"
 
-when not declared(AsyncCond):
-    import "./concurrency.nim"
+when not defined(standalone):
+    when not declared(AsyncCond):
+        import "./concurrency.nim"
 
 when declared(bconsole):
     include "./console.nim"
