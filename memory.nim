50039f2c23dba239619fb8c551342312a638f38b
mbourke525 <mitchell.bourke@ausproof.com.au>
2025-08-25 15:22:52 +1000

    .

---
+++ memory.nim
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+#                                    Implements macros and utility functions for memory managment and GC operations
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Defines a macro for simplified definitions of destroy functions for both ref
+template destructor(t: typedesc, body: untyped): untyped =
+    when t is ref:
+        proc `=destroy`(x: typeof `t`()[]) =
+            `body`
+    else:
+        proc `=destroy`(x: `t`) =
+            `body`
