840adceaf15dc6b0225cfcd7c5034c1dc50ed29e
mbourke525 <mitchell.bourke@ausproof.com.au>
2025-11-24 10:03:16 +1000

    .

--- memory.nim
+++ memory.nim
@@ -2,17 +2,21 @@
 #                                    Implements macros and utility functions for memory managment and GC operations
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#Defines a macro for simplified definitions of destroy functions for both ref
-template destructor(t: typedesc, body: untyped): untyped =
-    when t is ref:
-        proc `=destroy`(x: typeof `t`()[]) =
-            `body`
-    else:
-        proc `=destroy`(x: `t`) =
-            `body`
-
 #Macro that wraps a block of code in a gcsafe block
 macro gcSafe(body: untyped): untyped =
     result = quote do:
         {.cast(gcsafe).}:
             `body`
+
+#Defines a macro for simplified definitions of destroy functions for both ref
+macro destructor(t: typedesc, body: untyped): untyped =
+    let x = newIdentNode("x")
+    
+    when t is ref:
+        result = quote do:
+            proc `=destroy`(`x`: var typeof `t`()[]) =
+                `body`
+    else:
+        result = quote do:
+            proc `=destroy`(`x`: var `t`) =
+                `body`
