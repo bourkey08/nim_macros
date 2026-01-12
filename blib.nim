a621ca79b3499e5c402b0434f71c5c7a44144756
mbourke525 <mitchell.bourke@ausproof.com.au>
2026-01-13 09:36:29 +1000

    chor: fix pFor import

--- blib.nim
+++ blib.nim
@@ -25,7 +25,7 @@ when not declared(with):
         when not defined(standalone):
             when not declared(AsyncCond):
                 import "./concurrency.nim"
-            when defined(threads):
+            when declared(Thread):
                 include "./parallel_utils.nim"
 
     #The standard library time functions dont work properly on 8bit microcontrollers, i havent tested them on 16bit but expect this to fail as well due to assumption that int can be used to store an i32
