b3c99a75b581b2871d3dd4e679d3b9cb1c96f6a8
mbourke525 <mitchell.bourke@ausproof.com.au>
2026-01-14 11:19:42 +1000

    chor: Add macro for defining unsafe (memory) operations

--- memory.nim
+++ memory.nim
@@ -2,7 +2,7 @@
 #                                    Implements macros and utility functions for memory managment and GC operations
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 
-#Macro that wraps a block of code in a gcsafe block
+##Macro that wraps a block of code in a gcsafe block
 macro gcSafe(body: untyped): untyped =
     result = quote do:
         {.cast(gcsafe).}:
@@ -21,7 +21,25 @@ macro destructor(t: typedesc, body: untyped): untyped =
             proc `=destroy`(`x`: var `t`) =
                 `body`
 
-
+##Defines a + operation for pointers to allow pointer arithmetic
 macro `+`(a: pointer, b: int): pointer =
     result = quote do:
         cast[pointer](cast[int](`a`) + `b`)
+
+##Defines a - operation for pointers to allow pointer arithmetic
+macro `-`(a: pointer, b: int): pointer =
+    result = quote do:
+        cast[pointer](cast[int](`a`) - `b`)
+
+##Defines a macro for cleaner definitions of unsafe optimizations with a safe alternative
+#Eg copyMem(val.addr + 6, data.addr, val.len) or val[6..^1] = data)
+macro unsafeOpts(body: untyped, elsebody: untyped): untyped =
+    if elsebody.kind == nnkElse:
+        when defined(unsafeOpts):
+            result = quote do:
+                `body`
+        else:
+            #Strip the else node to get the actual body
+            let eBody = elsebody[0]
+            result = quote do:
+                `eBody`
