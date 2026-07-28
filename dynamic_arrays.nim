bd427d7ba286ff29f1e8fadb38fa3a64b2154889
mbourke525 <mitchell.bourke@ausproof.com.au>
2026-07-28 14:00:26 +1000

    feat: Add dArray type for runtime sized heap allocated arrays

---
+++ dynamic_arrays.nim
@@ -0,0 +1,54 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+#                                        Implements fixed length heap allocated arrays with a size set at runtime
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#---------------------------------------------- Types ----------------------------------------------
+#Type for a heap allocated array with a sized set at runtime
+type dArray*[T] = ref object
+    size: int
+    freed: bool = false#Flag that is set to true once the array has been freed from memory
+    data: ptr UncheckedArray[T]
+
+#----------------------------------- Constructors and Destructors ----------------------------------
+#Allocate a new dynamic array of a given size and type
+proc newdArray*[T](size: int): dArray[T] =
+    # Define a destructor for this specific type of the dynamic array to free its memory when it goes out of scope
+    proc `=destroy`(x: var typeof dArray[T]()[]) =
+        if not x.freed:
+            dealloc(cast[pointer](x.data))
+            x.freed = true
+
+    #Create an object for the dynamically allocated array
+    result = dArray[T](
+        size: size,
+        data: cast[ptr UncheckedArray[T]](alloc(size * sizeof(T)))
+    )
+
+## Called to explicitly free the memory used by the dynamic array
+proc destroy*[T](self: dArray[T]) =
+    ## Frees the memory used by the dynamic array
+    if not self.freed:
+        dealloc(cast[pointer](self.data))
+        self.freed = true
+
+## Alias for destroy to free the memory used by the dynamic array
+proc free*[T](self: dArray[T]) =
+    ## Frees the memory used by the dynamic array
+    self.destroy()
+
+#------------------------- Properties for accessing data on the array type -------------------------
+## Returns the element at the given index in the array
+template `[]`*[T](self: dArray[T], index: int): T =
+    if index < 0 or index >= self.size:
+        raise newException(IndexError, "Index out of bounds")
+    self.data[index]
+
+## Sets the element at the given index in the array to the given value
+template `[]=`*[T](self: dArray[T], index: int, value: T) =
+    if index < 0 or index >= self.size:
+        raise newException(IndexError, "Index out of bounds")
+    self.data[index] = value
+
+## Returns the total number of allocated elements in the array (not the number of elements stored or the byte size of the array)
+proc `len`*[T](self: dArray[T]): int {.inline.} =
+    return self.size
