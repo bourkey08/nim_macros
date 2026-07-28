c1e9d5506ad9852624403983b669074eb2cfb59a
mbourke525 <mitchell.bourke@ausproof.com.au>
2026-07-28 12:05:28 +1000

    add inThread macro

--- parallel_utils.nim
+++ parallel_utils.nim
@@ -141,9 +141,6 @@ macro pMap(data: untyped, body: untyped) : untyped =
     result.add quote do:
         initBPool()
 
-    #Split the data into the variable and data seq
-    echo treeRepr(data)
-
     if data[0].kind != nnkIdent or data[0].strVal != "in":
         raise newException(ValueError, "parallelUtils.nim pMap macro expects 'entry in data' syntax")
 
@@ -203,3 +200,46 @@ macro pMap(data: untyped, body: untyped) : untyped =
 
         #Return the output data seq
         `outDataIdent`
+
+
+#Macros for transforming sync calls into async calls via the thread pool
+#Implements a macro that allows a block of code to be run in a thread and awaited using a future
+#This is used to allow async interfaces to be used in threaded code that is not async aware
+#Note: This should not be used in a loop as it will cause issues with multiple completions of a single future
+macro inThread(body: untyped): Future[void] =
+    bPoolFuncCounter.inc()
+    result = newStmtList()
+
+    #Ensure the thread pool is initialized
+    result.add quote do:
+        initBPool()
+
+    #Define identifiers used in the generated code to ensure unique names for each macro call and avoid name collisions
+    let poolName = newIdentNode("bThreadPoolGInst")
+    let bodyIndent = newIdentNode("body_" & $bPoolFuncCounter.value)
+    let wrapper = newIdentNode("wrapper_" & $bPoolFuncCounter.value)        
+
+    #Build the body method that is called by the thread pool
+    result.add quote do:
+        var respFut: Future[void] = newFuture[void]()
+
+        proc `bodyIndent`(index: int) =
+            gcSafe:
+                `body`
+
+            respFut.complete()
+
+        #Now dispatch the job to the thread pool
+        let idx: int = 0
+        var jobEntry: tuple[f: proc(index: int), i: int] = (`bodyIndent`, idx)
+        `poolName`.chann.send(jobEntry)
+
+        #Return the future to the caller
+        proc `wrapper`(baseFut: Future[void]): Future[void] {.async.} =
+            while true:       
+                if baseFut.finished:
+                    break
+                else:
+                    await sleepAsync(1)
+
+        `wrapper`(respFut)
